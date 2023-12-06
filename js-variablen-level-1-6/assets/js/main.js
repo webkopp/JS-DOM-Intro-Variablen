6a9b982f7f415ac9ec0f96e0273f6fdf08318b12
webkopp <nakopps@googlemail.com>
2023-12-06 17:58:11 +0100

    fertig

--- js-variablen-level-1-6/assets/js/main.js
+++ js-variablen-level-1-6/assets/js/main.js
@@ -1,2 +1,12 @@
 // 1. Deklariere die Variable let x mit dem Wert 34. Überschreibe x anschließend mit dem Wert 67. Gib den Wert x in der Konsole aus. Danach kommentiere deinen Code aus.
 // 2. Deklariere die Variable const y mit dem Wert 34. Überschreibe y anschließend mit dem Wert 67. Gib den Wert y in der Konsole aus. Warum erhältst du eine Fehlermeldung?
+
+// let x = 34;
+// x = 67;
+// console.log({x});
+
+const y = 34;
+y = 67;
+console.log({y});
+
+weil es eine constante ist und kein let
